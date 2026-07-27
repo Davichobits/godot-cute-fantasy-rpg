0b9b63c0d1c32aa6635fb350e6cf97e1693114d0
David Ruiz <davidruizdeveloper@gmail.com>
2026-07-27 17:18:14 -0500

    player movement and animations

--- player.gd
+++ player.gd
@@ -1,21 +1,42 @@
 extends CharacterBody2D
 
-@onready var animated_sprite_2d = $AnimatedSprite2D
 
 const SPEED = 300.0
 
-func _physics_process(delta):
+var last_direction: Vector2 = Vector2.RIGHT
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(_delta: float) -> void:
 	process_movement()
+	process_animation()
 	move_and_slide()
+
 
 func process_movement() -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_vector("left", "right", "up", "down")
+	var direction := Input.get_vector("left", "right", "up", "down")
 	
-	velocity = direction * SPEED
-	play_animation(direction)
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+		last_direction = direction
+	else:
+		velocity = Vector2.ZERO
 	
-func play_animation(dir: Vector2) -> void:
-	if dir.x > 0:
-		animated_sprite_2d.play("idle_right")
+	
+func process_animation() -> void:
+	if velocity != Vector2.ZERO:
+		play_animation("run", last_direction)
+	else:
+		play_animation("idle", last_direction)
+
+func play_animation(prefix: String, dir: Vector2) -> void:
+	if dir.x != 0:
+		animated_sprite_2d.flip_h = dir.x < 0
+		animated_sprite_2d.play(prefix + "_right")
+	elif dir.y < 0:
+		animated_sprite_2d.play(prefix + "_up")
+	elif dir.y > 0:
+		animated_sprite_2d.play(prefix + "_down")
